0194b269ddf20846e33125eec604303607eaa860
Kyle Benson <kyle.edward.benson@gmail.com>
2013-10-03 01:18:38 -0700

    finished hw1, made PDFs also allowed

---
+++ hw1/mydist.m
@@ -0,0 +1,10 @@
+function D = dist(x, X)
+% Calculates the vector of Euclidean distances between a single vector x
+% and a collection of data points (stored in a matrix) X.
+
+differences = bsxfun(@minus, x, X);
+squared_differences = differences.^2;
+sum_squared_differences = sum(squared_differences, 2);
+D = sqrt(sum_squared_differences);
+
+end
