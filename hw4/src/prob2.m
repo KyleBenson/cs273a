704eeb7f24802e2dd15227f29420e2391ce3a140
Kyle Benson <kyle.edward.benson@gmail.com>
2013-11-20 01:50:59 -0800

    finished 3, almost done 2, and added some to the report

--- hw4/src/prob2.m
+++ hw4/src/prob2.m
@@ -1,6 +1,6 @@
 problem = 'b'
 
-iris=load('~/repos/cs273a/hw4/src/data/iris.txt');
+iris=load('data/iris.txt');
 X = iris(:,1:2); Y=iris(:,end);
 XA = X(Y<2,:); YA=Y(Y<2); % 0 vs 1
 YA = (YA.*2)-1; % set to -1,1
@@ -35,17 +35,19 @@ Xsquare = XA * XA';
 H = Ysquare.*Xsquare;
 f = -1*ones(N,1);
 
-Aeq = diag(YA);
-beq = zeros(dim,1);
+%Aeq = diag(YA);
+Aeq = YA';
+%beq = zeros(dim,1);
+beq = [0];
 
-theta = quadprog(H,f,[],[],Aeq, beq,-100000*ones(N,1),100000*ones(N,1))
+theta = quadprog(H,f,[],[],Aeq, beq,zeros(N,1),100000*ones(N,1))
 
-% separate parameters
-%b = theta(1);
-%theta = theta(2:end);
+% theta is a N-dim vector of alpha weights,
+% so we need to extract the actual theta parameters from it
+%TODO
 
 pc = perceptClassify(XA, YA);
-pc = setWeights(pc, theta');
+pc = setWeights(pc, [0 theta']');
 plotClassify2D(pc, XA, YA);
 
 end;
--- hw4/src/prob3.m
+++ hw4/src/prob3.m
@@ -1,11 +1,13 @@
 problem = 'd'
 
-rand('state',0); randn('state',0);
+%rand('state',0); randn('state',0);
 data = load('data/spambase.data');
 [N,D] = size(data);
 X = data(:,1:end-1); Y=data(:,end);
 [X,Y] = shuffleData(X,Y);
 [Xt,Xv,Yt,Yv] = splitData(X,Y,.6);
+Yt(Yt == 0) = -1;
+Yv(Yv == 0) = -1;
 Nt = size(Yt, 1)
 Nv = size(Yv, 1)
 [Xt,S] = rescale(Xt); Xv = rescale(Xv,S);
@@ -44,42 +46,53 @@ elseif problem == 'b'
     hold off;
 
 elseif problem == 'c'
-    trainingErrors = zeros(size(data, 2), 1);
-    testErrors = zeros(size(data, 2), 1);
-
-    maxDepth = 100;
-    depths = 1:maxDepth;
-    trainingErrors = zeros(size(depths));
-    testErrors = zeros(size(depths));
-
+    maxDepth = 15;
+    %maxDepth = 100;
+    depths = round(logspace(0,log10(maxDepth)));
+    maxDepth = size(depths, 2); % so that we create prediction
+                                % vectors of the right size
+    
     %we collect the sum of all predictions to average them later to
     %take majority vote
     YtHat = zeros(maxDepth,Nt);
     YvHat = zeros(maxDepth,Nv);
 
+    depthItr = 1;
     for depth = depths    
 
         for itr = 1:depth
             %bootstrap data
             [XtData YtData] = bootstrapData(Xt, Yt, Nt);
 
-            tree = treeClassify(XtData, YtData, 0, depth, .001, inf);
+            tree = treeClassify(XtData, YtData, 0, inf, 0, inf);
 
             %run each classifier, summing up the predictions for an
             %average later
-            YtHat(depth,:) = YtHat(depth,:) + predict(tree, Xt)'./depth;
-            YvHat(depth,:) = YvHat(depth,:) + predict(tree, Xv)'./depth;
+            % treeClassify outputs 0/1 labels, so change to -1/+1
+            tpredictions = predict(tree, Xt);
+            vpredictions = predict(tree, Xv);
+            YtHat(depthItr,:) = YtHat(depthItr,:) + tpredictions'./depth;
+            YvHat(depthItr,:) = YvHat(depthItr,:) + vpredictions'./depth;
             
         end;
-
+        depthItr = depthItr + 1;
     end;
 
     %majority vote
     tmajority = sign(YtHat);
     vmajority = sign(YvHat);
 
-    trainingErrors = (Nt - sum(YtHat == repmat(Yt', maxDepth, 1), 2)) ./ Nt;
-    testErrors = (Nv - sum(YvHat == repmat(Yv', maxDepth, 1), 2)) ./ Nv;
+    tmajority(tmajority == 0) = 1;
+    vmajority(vmajority == 0) = 1;
+    
+    tmajority = bsxfun(@eq, tmajority, Yt');
+    vmajority = bsxfun(@eq, vmajority, Yv');
+    
+    tmajority = sum(tmajority, 2); % get # right for each depth
+    vmajority = sum(vmajority, 2); % get # right for each depth
+
+    trainingErrors = (Nt - tmajority) ./ Nt;
+    testErrors = (Nv - vmajority) ./ Nv;
 
     %plot errors vs. depth    
 
@@ -92,39 +105,52 @@ elseif problem == 'c'
     hold off;
 
 elseif problem == 'd'
-    maxDepth = 25;
-
+    nitr = 25;
+    testPoints = 100;
+    
     %we collect the sum of all predictions to average them later to
     %take majority vote
-    YtHat = zeros(maxDepth,Nt);
-    YvHat = zeros(maxDepth,Nv);
-
-    depth = maxDepth;
-    nitr = depth;
-    nSamplesToTest = floor(logspace(log10(round(0.1*Nt)), log10(Nt)));
+    YtHat = zeros(testPoints,Nt);
+    YvHat = zeros(testPoints,Nv);
+    
+    nSamplesToTest = floor(logspace(log10(round(0.1*Nt)), log10(Nt), testPoints));
+    testItr = 1;
     for nSamples = nSamplesToTest    
 
         for itr = 1:nitr
             %bootstrap data
             [XtData YtData] = bootstrapData(Xt, Yt, nSamples);
 
-            tree = treeClassify(XtData, YtData, 0, depth, .001, inf);
+            tree = treeClassify(XtData, YtData, 0, inf, 0, inf);
 
             %run each classifier, summing up the predictions for an
             %average later
-            YtHat(depth,:) = YtHat(depth,:) + predict(tree, Xt)';
-            YvHat(depth,:) = YvHat(depth,:) + predict(tree, Xv)';
+            % treeClassify outputs 0/1 labels, so change to -1/+1
+            tpredictions = predict(tree, Xt);
+            vpredictions = predict(tree, Xv);
+            YtHat(testItr,:) = YtHat(testItr,:) + tpredictions';
+            YvHat(testItr,:) = YvHat(testItr,:) + vpredictions';
             
         end;
-
+        testItr = testItr + 1;
     end;
 
     %majority vote
     tmajority = sign(YtHat./depth);
     vmajority = sign(YvHat./depth);
 
-    trainingErrors = (Nt - sum(YtHat == repmat(Yt', maxDepth, 1), 2)) ./ Nt;
-    testErrors = (Nv - sum(YvHat == repmat(Yv', maxDepth, 1), 2)) ./ Nv;
+    % prefer class 1 when tied
+    tmajority(tmajority == 0) = 1;
+    vmajority(vmajority == 0) = 1;
+    
+    tmajority = bsxfun(@eq, tmajority, Yt');
+    vmajority = bsxfun(@eq, vmajority, Yv');
+    
+    tmajority = sum(tmajority, 2); % get # right for each depth
+    vmajority = sum(vmajority, 2); % get # right for each depth
+
+    trainingErrors = (Nt - tmajority) ./ Nt;
+    testErrors = (Nv - vmajority) ./ Nv;
 
     %plot errors vs. nSamplesToTest    
 
